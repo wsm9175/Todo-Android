b5c0d9e86ddb6bb6527a51ce68c0fccabc694214
wsm9175 <wsm9175@naver.com>
2023-04-27 15:28:18 +0900

    add onToggle & onEdit & onDelete

--- app/src/main/java/com/wsm9175/todo/MainActivity.kt
+++ app/src/main/java/com/wsm9175/todo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.wsm9175.todo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,7 +26,127 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    TopLevel()
+                }
+            }
+        }
+    }
+}
 
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun TopLevel() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val toDoList = remember { mutableStateListOf<ToDoData>() }
+
+    val onSubmit: (String) -> Unit = { text ->
+        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+        toDoList.add(ToDoData(key, text))
+        setText("")
+    }
+
+    val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(done = checked)
+    }
+
+    val onEdit: (Int, String) -> Unit = { key, text ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList[i] = toDoList[i].copy(text = text)
+    }
+
+    val onDelete: (Int) -> Unit = { key ->
+        val i = toDoList.indexOfFirst { it.key == key }
+        toDoList.removeAt(i)
+    }
+
+    Scaffold() {
+        Column() {
+            ToDoInput(
+                text = text,
+                onTextChange = setText,
+                onSubmit = onSubmit
+            )
+
+            LazyColumn {
+                items(toDoList, key = { it.key }) { toDoData ->
+                    ToDo(
+                        toDoData = toDoData,
+                        onToggle = onToggle,
+                        onDelete = onDelete,
+                        onEdit = onEdit
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ToDo(
+    toDoData: ToDoData,
+    onEdit: (key: Int, text: String) -> Unit = { _, _ -> },
+    onToggle: (key: Int, checked: Boolean) -> Unit = { _, _ -> },
+    onDelete: (key: Int) -> Unit = {}
+) {
+    var isEditing by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier.padding(4.dp),
+        elevation = 8.dp
+    ) {
+        Crossfade(targetState = isEditing) {
+            when (it) {
+                false -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text(
+                            text = toDoData.text,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Text(
+                            text = "완료"
+                        )
+                        Checkbox(
+                            checked = toDoData.done,
+                            onCheckedChange = { checked ->
+                                onToggle(toDoData.key, checked)
+                            }
+                        )
+                        Button(onClick = {
+                            isEditing = true
+                        }) {
+                            Text(text = "수정")
+                        }
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(onClick = {
+                            onDelete(toDoData.key)
+                        }) {
+                            Text(text = "삭제")
+                        }
+                    }
+                }
+                true -> {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        var (newText, setNewText) = remember { mutableStateOf(toDoData.text) }
+                        OutlinedTextField(
+                            value = newText,
+                            onValueChange = setNewText,
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.size(4.dp))
+                        Button(onClick = {
+                            onEdit(toDoData.key, newText)
+                            isEditing = false
+                        }) {
+                            Text(text = "완료")
+                        }
+                    }
                 }
             }
         }
@@ -33,10 +158,10 @@ fun ToDoInput(
     text: String,
     onTextChange: (String) -> Unit,
     onSubmit: (String) -> Unit,
-){
+) {
     Row(
         Modifier.padding(8.dp)
-    ){
+    ) {
         OutlinedTextField(
             value = text,
             onValueChange = onTextChange,
@@ -58,9 +183,17 @@ fun ToDoInput(
 
 @Preview(showBackground = true)
 @Composable
-fun ToDoInputPreview(){
+fun ToDoInputPreview() {
     TODOTheme {
         ToDoInput("테스트", {}, {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoDataPreview() {
+    TODOTheme {
+        ToDo(toDoData = ToDoData(1, "nice", true))
     }
 }
 
@@ -69,5 +202,12 @@ fun ToDoInputPreview(){
 @Composable
 fun DefaultPreview() {
     TODOTheme {
+        TopLevel()
     }
 }
+
+data class ToDoData(
+    val key: Int,
+    val text: String,
+    val done: Boolean = false
+)
